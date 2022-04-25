31c54ba58dd4eab009bdf0260c7ea94efa8747df
Ed Kubiak <ek33450505@gmail.com>
2022-04-24 22:15:44 -0400

    added role table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,14 @@
 DROP TABLE IF EXISTS department;
+DROP TABLE if EXISTS role;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL(10,2),
+    department_id INTEGER
 );
