4f965f6efdcf2be0d031e838ddea1b071ecf4bfa
Ed Kubiak <ek33450505@gmail.com>
2022-04-25 11:33:58 -0400

    finished role api requests

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.delete('/api/department/:id', (req, res) => {
 app.post('/api/department', ({ body }, res) => {
     const errors = inputCheck(
       body,
-      'name',
+      'name'
     );
     if (errors) {
       res.status(400).json({ error: errors });
@@ -104,7 +104,89 @@ app.post('/api/department', ({ body }, res) => {
       });
     });
   });
+
+  // Get all roles
+  app.get('/api/role', (req, res) => {
+    const sql = `SELECT * FROM role`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  // Get a single role by id
+  app.get('/api/role/:id', (req, res) => {
+    const sql = `SELECT * FROM role WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+  // Delete a single role
+  app.delete('/api/role/:id', (req, res) => {
+    const sql = `DELETE FROM role WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        // checks if anything was deleted
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'role not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+  // Add a new role
+app.post('/api/role', ({ body }, res) => {
+    const errors = inputCheck(
+      body,
+      'title', 'salary', 'department_id'
+    );
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
   
+    const sql = `INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`;
+    const params = [
+      body.title,
+      body.salary,
+      body.department_id
+    ];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
