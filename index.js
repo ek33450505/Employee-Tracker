ee3b69c3c9d7bec163c130eccee1cad37ce5b6ef
Ed Kubiak <ek33450505@gmail.com>
2022-04-28 18:21:07 -0400

    had to rethink this project a bit. Glad that I veified routes, but unnecessary

---
+++ index.js
@@ -0,0 +1,110 @@
+const inquirer = require ("inquirer");
+const mysql = require("mysql2");
+const ctable = require('console.table');
+
+// connection to database
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'VanLouis15!',
+  database: 'business'
+});
+
+// function which prompts the user for what action they should take
+const userPrompt = () => {
+  inquirer.prompt ([
+    {
+      type: 'list',
+      name: 'choices', 
+      message: 'What would you like to do?',
+      choices: ['View all departments', 
+                'View all roles', 
+                'View all employees', 
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update an employee role',
+                'Update an employee manager',
+                "View employees by department",
+                'Delete a department',
+                'Delete a role',
+                'Delete an employee',
+                'View department budgets',
+                'No Action']
+    }
+  ])
+    .then((answers) => {
+      const { choices } = answers; 
+
+      if (choices === "View all departments") {
+        showDepartments();
+      }
+
+      if (choices === "View all roles") {
+        showRoles();
+      }
+
+      if (choices === "View all employees") {
+        showEmployees();
+      }
+
+      if (choices === "Add a department") {
+        addDepartment();
+      }
+
+      if (choices === "Add a role") {
+        addRole();
+      }
+
+      if (choices === "Add an employee") {
+        addEmployee();
+      }
+
+      if (choices === "Update an employee role") {
+        updateEmployee();
+      }
+
+      if (choices === "Update an employee manager") {
+        updateManager();
+      }
+
+      if (choices === "View employees by department") {
+        employeeDepartment();
+      }
+
+      if (choices === "Delete a department") {
+        deleteDepartment();
+      }
+
+      if (choices === "Delete a role") {
+        deleteRole();
+      }
+
+      if (choices === "Delete an employee") {
+        deleteEmployee();
+      }
+
+      if (choices === "View department budgets") {
+        viewBudget();
+      }
+
+      if (choices === "No Action") {
+        connection.end()
+    };
+  });
+};
+userPrompt()
+
+// const addDepartment = () => {
+//     return inquirer.prompt([])};
+
+// const addRole = () => {
+//     return inquirer.prompt([])};
+
+// const addEmployee = () => {
+//     return inquirer.prompt([])};
+
+// const updateEmployeeRole = () => {
+//     return inquirer.prompt([])};
